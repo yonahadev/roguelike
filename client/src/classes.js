f8bef6c8b307cdfa2cfad610aa2916f79d1fdbf4
yonahadev <83908974+yonahadev@users.noreply.github.com>
2024-07-08 19:35:11 +0100

    vector class + seperate file and folder

---
+++ client/src/classes.js
@@ -0,0 +1,10 @@
+class vector2 { 
+  constructor(x, y) { 
+    this.x = x
+    this.y = y
+  }
+  move(x, y) { 
+    this.x += x
+    this.y += y
+  }
+}
--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,19 @@
 let canvas = document.getElementById("canvas")
 let context = canvas.getContext("2d")
-context.fillStyle = "blue"
+
+if (context) {
+  context.fillStyle = "blue"
+} else { 
+  console.warn("CANVAS CONTEXT NOT FOUND");
+}
 
 
-let moving
 let inputQueue = new Set()
 let movementKeys = new Set(["w", "a", "s", "d"])
 
-let position = [0,0]
+let position = new vector2(0, 0)
 
-let lastTime = new Date().getTime()
+const playerSpeed = 2
 
 document.addEventListener("keydown", (event) => { 
   if (movementKeys.has(event.key)) { 
@@ -19,51 +23,51 @@ document.addEventListener("keydown", (event) => {
 
 document.addEventListener("keyup", (event) => { 
   if (movementKeys.has(event.key)) { 
-    inputQueue.delete(event.key )
+    inputQueue.delete(event.key)
   }
 })
 
 let handleMovement = () => { 
   let input1 = [...inputQueue][0]
   let input2 = [...inputQueue][1]
+  const diagonalSpeed = playerSpeed/Math.sqrt(2)
   switch (true) { 
     case (input1 == "w" && input2 == undefined):
-      position[1] -= Math.sqrt(2)
+      position.move(0,-playerSpeed)
       break
     case (input1 == "a" && input2 == undefined):
-      position[0] -= Math.sqrt(2)
+      position.move(-playerSpeed,0)
       break
     case (input1 == "s" && input2 == undefined):
-      position[1] += Math.sqrt(2)
+      position.move(0,playerSpeed)
       break
     case (input1 == "d" && input2 == undefined):
-      position[0] += Math.sqrt(2)
+      position.move(playerSpeed,0)
       break
     case (input1 == "w" && input2 == "d" || input2 == "w" && input1 == "d"):
-      position[1] -= 1
-      position[0] += 1
+      position.move(diagonalSpeed,-diagonalSpeed)
       break
     case (input1 == "w" && input2 == "a" || input2 == "w" && input1 == "a"):
-      position[1] -= 1
-      position[0] -= 1
+      position.move(-diagonalSpeed,-diagonalSpeed)
       break
     case (input1 == "s" && input2 == "a" || input2 == "s" && input1 == "a"):
-      position[1] += 1
-      position[0] -= 1
+      position.move(-diagonalSpeed,diagonalSpeed)
       break
     case (input1 == "s" && input2 == "d" || input2 == "s" && input1 == "d"):
-      position[1] += 1
-      position[0] += 1
+      position.move(diagonalSpeed,diagonalSpeed)
       break
   }
 }
 
-
 let renderFunction = (time) => { 
-  window.requestAnimationFrame(renderFunction)
-  context.clearRect(0, 0, 400, 400)
-  handleMovement()
-  context.fillRect(position[0],position[1], 100, 100)
+  if (context) { 
+    window.requestAnimationFrame(renderFunction)
+    context.clearRect(0, 0, 400, 400)
+    handleMovement()
+    context.fillRect(position.x, position.y, 100, 100)
+
+  }
+
 }
 
 
