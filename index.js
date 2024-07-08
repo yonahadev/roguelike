5b1484b2e6a364ebf97dea0f153072c28bae8ddc
yonahadev <83908974+yonahadev@users.noreply.github.com>
2024-07-08 20:13:22 +0100

    Express + socket.io init

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require("path")
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+app.use(express.static(path.join(__dirname,'client')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'client','index.html'))
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+
+server.listen(3000, () => {
+  console.log('listening on localhost:3000');
+});
