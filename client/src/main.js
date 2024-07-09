0491ffbf25f5e0408d79adb8a7b489d9c79d59f4
yonahadev <83908974+yonahadev@users.noreply.github.com>
2024-07-09 12:10:28 +0100

    sorted out positioning sending at interval

--- client/src/main.js
+++ client/src/main.js
@@ -1,29 +1,25 @@
 let canvas = document.getElementById("canvas")
 let context = canvas.getContext("2d")
-
-let CANVAS_WIDTH = window.innerWidth
-let CANVAS_HEIGHT = window.innerHeight
-
-let resizeCanvas = (width,height) => {
-  canvas.width = width
-  canvas.height = height
-}
-
-if (context) {
-  context.fillStyle = "blue"
-} else { 
-  console.warn("CANVAS CONTEXT NOT FOUND");
-}
-
 let inputQueue = new Set()
 let movementKeys = new Set(["w", "a", "s", "d"])
-let position = new vector2(0, 0)
+let localPosition = new vector2(0, 0)
 const PLAYER_SPEED = 20
 let socket = io()
 let lastSendTime = new Date().getTime()
 let lastUpdateTime = new Date().getTime()
-let playerPositions = {}
+let playerData = {}
 let localID
+
+let resizeCanvas = () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+}
+
+let drawPlayer = (x,y) => { 
+  context.fillRect(x, y, 100, 100)
+}
+
+window.addEventListener('resize', resizeCanvas, false)
 
 document.addEventListener("keydown", (event) => { 
   if (movementKeys.has(event.key)) { 
@@ -43,72 +39,67 @@ let handleMovement = () => {
   const diagonalSpeed = PLAYER_SPEED/Math.sqrt(2)
   switch (true) { 
     case (input1 == "w" && input2 == undefined):
-      position.move(0,-PLAYER_SPEED)
+      localPosition.move(0,-PLAYER_SPEED)
       break
     case (input1 == "a" && input2 == undefined):
-      position.move(-PLAYER_SPEED,0)
+      localPosition.move(-PLAYER_SPEED,0)
       break
     case (input1 == "s" && input2 == undefined):
-      position.move(0,PLAYER_SPEED)
+      localPosition.move(0,PLAYER_SPEED)
       break
     case (input1 == "d" && input2 == undefined):
-      position.move(PLAYER_SPEED,0)
+      localPosition.move(PLAYER_SPEED,0)
       break
     case (input1 == "w" && input2 == "d" || input2 == "w" && input1 == "d"):
-      position.move(diagonalSpeed,-diagonalSpeed)
+      localPosition.move(diagonalSpeed,-diagonalSpeed)
       break
     case (input1 == "w" && input2 == "a" || input2 == "w" && input1 == "a"):
-      position.move(-diagonalSpeed,-diagonalSpeed)
+      localPosition.move(-diagonalSpeed,-diagonalSpeed)
       break
     case (input1 == "s" && input2 == "a" || input2 == "s" && input1 == "a"):
-      position.move(-diagonalSpeed,diagonalSpeed)
+      localPosition.move(-diagonalSpeed,diagonalSpeed)
       break
     case (input1 == "s" && input2 == "d" || input2 == "s" && input1 == "d"):
-      position.move(diagonalSpeed,diagonalSpeed)
+      localPosition.move(diagonalSpeed,diagonalSpeed)
       break
-  }
-  if (localID) { 
-    playerPositions[localID] = position
   }
 }
 
 socket.on('id', (id) => { 
   localID = id
-  console.log(localID)
+  console.log('localID:',localID)
 })
-
-socket.on('playerPosition', ({id,vector2}) => { 
-  playerPositions[id] = vector2
-})
-
-socket.on('playerLeft', (id) => { 
-  delete playerPositions[id]
+socket.on('playerData', (playerDataFromServer) => { 
+  playerData = playerDataFromServer
 })
 
 let renderFunction = (time) => { 
-  if (context) { 
-    window.requestAnimationFrame(renderFunction)
-    context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    Object.entries(playerPositions).forEach(([key, value]) => { 
-      if (value) { 
-        context.fillRect(value.x, value.y, 100, 100)
+  window.requestAnimationFrame(renderFunction)
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  drawPlayer(localPosition.x, localPosition.y)
+  Object.entries(playerData).forEach(([playerID, data]) => {  
+    if (playerID != localID && playerID && data) { 
+      let position = data["position"]
+      if (position) { 
+        drawPlayer(position.x,position.y)
       }
-    })
-    let currentTime = new Date().getTime()
-    let timeSinceUpdate = currentTime - lastUpdateTime
-    let timeSinceSend = currentTime - lastSendTime
-    if (timeSinceSend > 16) { 
-      lastSendTime = currentTime
-      socket.emit('playerPosition',position)
     }
-    if (timeSinceUpdate > 20) { 
-      lastUpdateTime = currentTime
-      handleMovement()
-      console.log(time)
-    }
+  })
 
+  let currentTime = new Date().getTime()
+  let timeSinceUpdate = currentTime - lastUpdateTime
+  let timeSinceSend = currentTime - lastSendTime
+  if (timeSinceSend > 16) { 
+    console.log("sent data to server",time)
+    lastSendTime = currentTime
+    socket.emit('playerPosition',localPosition)
   }
+  if (timeSinceUpdate > 20) { 
+    lastUpdateTime = currentTime
+    handleMovement()
+  }
+
 }
 
-
+resizeCanvas()
 window.requestAnimationFrame(renderFunction)
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const path = require("path")
-
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -8,6 +7,9 @@ const { Server } = require("socket.io")
 const io = new Server(server)
 const PORT = process.env.PORT
 
+//format of playerData[player][position] etc
+let playerData = {}
+
 app.use(express.static(path.join(__dirname,'client')))
 
 app.get('/', (req, res) => {
@@ -15,22 +17,33 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log(socket.id,'connected')
-
-  socket.emit('id', socket.id)
+  console.log(socket.id, 'connected')
+  
+  socket.emit('id', socket.id) 
   
   socket.on('playerPosition', (position) => { 
-    socket.broadcast.emit('playerPosition', {id:socket.id,vector2:position})
+    console.log(position)
+    if (playerData[socket.id]) { 
+      playerData[socket.id]["position"] = position
+    } else {
+      playerData[socket.id] = {}
+      playerData[socket.id]["position"] = position
+    }
   })
   
   socket.on('disconnect', () => { 
     console.log(socket.id, 'disconnected')
-    socket.broadcast.emit('playerLeft', socket.id)
   })
 })
-
-
 
 server.listen(PORT, () => {
   console.log('listening on',PORT);
 });
+
+setInterval(() => { 
+  io.emit('playerData',playerData)
+}, 20)
+
+setInterval(() => {
+  console.log(playerData)
+}, 1000);
